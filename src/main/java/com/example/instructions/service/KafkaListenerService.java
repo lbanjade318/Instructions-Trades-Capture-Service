15bd746b9f6a36f6377e4920f28b46c0bcea2c40
lbanjade318 <lbanjade318@gmail.com>
2025-11-07 14:31:37 -0500

    update Listener

--- src/main/java/com/example/instructions/service/KafkaListenerService.java
+++ src/main/java/com/example/instructions/service/KafkaListenerService.java
@@ -1,10 +1,10 @@
 package com.example.instructions.service;
 
-import com.example.instructions.model.Trade;
+
 import com.example.instructions.model.CanonicalTrade;
 import com.example.instructions.util.TradeTransformer;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class KafkaListenerService {
 
     private final TradeService tradeService;
-    private final ObjectMapper mapper = new ObjectMapper();
+
 
 
     public KafkaListenerService(TradeService tradeService) {
@@ -21,10 +21,21 @@ public class KafkaListenerService {
 
     @KafkaListener(topics ={"${kafka.topic.inbound}"} , groupId = "instructions-group")
     public void listen(CanonicalTrade ct) {
+        if(ct == null){
+            System.err.println("Received null trade message");
+            return;
+        }
         try {
-            tradeService.store(TradeTransformer.transform(ct));
+            var transformed = TradeTransformer.transform(ct);
+            tradeService.store(transformed);
+            //System.out.println("Trade successfully processed and stored: " + transformed.getPlatformId());
+
+        } catch (IllegalArgumentException e) {
+            System.err.println("Invalid trade data: " + e.getMessage());
         } catch (Exception e) {
+            e.printStackTrace();
             System.err.println("Failed to process inbound: " + e.getMessage());
         }
+
     }
 }
